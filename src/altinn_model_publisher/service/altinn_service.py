9da8870709ef943a78d2ea2c9cb86effd3ae842b
NilsOveTen <github.nils.ove@tendenes.com>
2020-10-23 12:11:26 +0200

    chore: add docstring lint rules

--- src/altinn_model_publisher/service/altinn_service.py
+++ src/altinn_model_publisher/service/altinn_service.py
@@ -1,7 +1,9 @@
+"""Service layer module for modelldcat-ap-no compliant information models from altinn."""
 from typing import Optional
 
 
 def is_ready(accept_header: Optional[str]) -> bool:
+    """Check if resource is available."""
     if accept_header == "application/json":
         return False
     else:
