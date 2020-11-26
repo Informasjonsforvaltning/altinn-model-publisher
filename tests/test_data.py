7ea529e66be3c91a26344379a700830dfbb455e5
NilsOveTen <github.nils.ove@tendenes.com>
2020-11-26 10:04:18 +0100

    fix: test

--- tests/test_data.py
+++ tests/test_data.py
@@ -20,49 +20,37 @@ def create_altinn_test_catalog() -> Catalog:
     )
 
     model_0 = InformationModel()
-    model_0.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3314-1212-245-10980"
-    )
+    model_0.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3314-245"
     model_0.title = {"nb": "Sensitive role Reporting"}
 
     model_1 = InformationModel()
-    model_1.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3357-130429-4166-35895"
-    )
+    model_1.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3357-4166"
     model_1.title = {"nb": "A02 a-melding submission from system"}
     model_1.publisher = model_publisher
 
     model_2 = InformationModel()
-    model_2.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3736-170607-4685-1706091129"
-    )
+    model_2.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3736-4685"
     model_2.title = {
         "nb": "A06 a-melding version 3.1.0 order reconciliation information"
     }
     model_2.publisher = model_publisher
 
     model_3 = InformationModel()
-    model_3.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3743-140516-4444-32407"
-    )
+    model_3.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3743-4444"
     model_3.title = {
         "nb": "A04 Declaration of paid work at home where a private individual is the employer"
     }
     model_3.publisher = model_publisher
 
     model_4 = InformationModel()
-    model_4.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3814-140602-4445-32408"
-    )
+    model_4.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3814-4445"
     model_4.title = {
         "nb": "A05 Simplified a-melding for charitable and non-profit organisations"
     }
     model_4.publisher = model_publisher
 
     model_5 = InformationModel()
-    model_5.identifier = (
-        "https://altinn.fellesdatakatalog.digdir.no/models/3906-141205-3940-20161021"
-    )
+    model_5.identifier = "https://altinn.fellesdatakatalog.digdir.no/models/3906-3940"
     model_5.title = {"nb": "A01 a-melding"}
     model_5.publisher = model_publisher
 
@@ -81,33 +69,33 @@ test_altinn_catalog_turtle = """
 <https://www.altinn.no/models/catalog> a dcat:Catalog ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/991825827> ;
     dct:title "Altinn informasjonsmodellkatalog"@nb ;
-    ns1:model <https://altinn.fellesdatakatalog.digdir.no/models/3314-1212-245-10980>,
-        <https://altinn.fellesdatakatalog.digdir.no/models/3357-130429-4166-35895>,
-        <https://altinn.fellesdatakatalog.digdir.no/models/3736-170607-4685-1706091129>,
-        <https://altinn.fellesdatakatalog.digdir.no/models/3743-140516-4444-32407>,
-        <https://altinn.fellesdatakatalog.digdir.no/models/3814-140602-4445-32408>,
-        <https://altinn.fellesdatakatalog.digdir.no/models/3906-141205-3940-20161021> .
+    ns1:model <https://altinn.fellesdatakatalog.digdir.no/models/3314-245>,
+        <https://altinn.fellesdatakatalog.digdir.no/models/3357-4166>,
+        <https://altinn.fellesdatakatalog.digdir.no/models/3736-4685>,
+        <https://altinn.fellesdatakatalog.digdir.no/models/3743-4444>,
+        <https://altinn.fellesdatakatalog.digdir.no/models/3814-4445>,
+        <https://altinn.fellesdatakatalog.digdir.no/models/3906-3940> .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3314-1212-245-10980> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3314-245> a ns1:InformationModel ;
     dct:title "Sensitive role Reporting"@nb .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3357-130429-4166-35895> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3357-4166> a ns1:InformationModel ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/974761076> ;
     dct:title "A02 a-melding submission from system"@nb .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3736-170607-4685-1706091129> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3736-4685> a ns1:InformationModel ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/974761076> ;
     dct:title "A06 a-melding version 3.1.0 order reconciliation information"@nb .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3743-140516-4444-32407> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3743-4444> a ns1:InformationModel ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/974761076> ;
     dct:title "A04 Declaration of paid work at home where a private individual is the employer"@nb .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3814-140602-4445-32408> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3814-4445> a ns1:InformationModel ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/974761076> ;
     dct:title "A05 Simplified a-melding for charitable and non-profit organisations"@nb .
 
-<https://altinn.fellesdatakatalog.digdir.no/models/3906-141205-3940-20161021> a ns1:InformationModel ;
+<https://altinn.fellesdatakatalog.digdir.no/models/3906-3940> a ns1:InformationModel ;
     dct:publisher <https://organization-catalogue.staging.fellesdatakatalog.digdir.no/organizations/974761076> ;
     dct:title "A01 a-melding"@nb .
 
